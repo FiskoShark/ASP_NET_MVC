9a11f8325a02214f04a43e4d198092ab9264b7d6
strongerko7-netizen <your-email@example.com>
2026-08-21 22:42:20 +0200

    add AnimalApp

---
+++ AnimalApp/AnimalApp/Controllers/AnimalController.cs
@@ -0,0 +1,34 @@
+﻿using AnimalApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalApp.Controllers{
+    public class AnimalController : Controller
+    {
+        private readonly List<Animal> animals =
+        [
+            new Animal { Id = 1, Name = "Мурчик", Species = "Кіт", Age = 3, Color = "Рудий" },
+    new Animal { Id = 2, Name = "Рекс", Species = "Собака", Age = 5, Color = "Чорний" },
+    new Animal { Id = 3, Name = "Барсик", Species = "Кіт", Age = 7, Color = "Сірий" },
+    new Animal { Id = 4, Name = "Бобік", Species = "Собака", Age = 2, Color = "Коричневий" }
+        ];
+
+        public IActionResult Index()
+        {
+            return View(animals);
+        }
+        public IActionResult Cards()
+        {
+            return View(animals);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var animal = animals.FirstOrDefault(x => x.Id == id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+            return View(animal);
+        }
+    }
+}
