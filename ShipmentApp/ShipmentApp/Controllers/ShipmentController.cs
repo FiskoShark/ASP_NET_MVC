1f59e4cc9b9261df03a907ce39be00ecd8c896ed
strongerko7-netizen <your-email@example.com>
2026-08-28 21:59:46 +0200

    add 2 homework

---
+++ ShipmentApp/ShipmentApp/Controllers/ShipmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShipmentApp.Models;
+
+namespace ShipmentApp.Controllers
+{
+    public class ShipmentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ShipmentViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View("Details", model);
+        }
+    }
+}
+
---
+++ ShipmentApp/ShipmentApp/Models/ShipmentViewMode.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShipmentApp.Models
+{
+    public class ShipmentViewModel
+    {
+        [Required]
+        public string RecipientName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+    }
+}
