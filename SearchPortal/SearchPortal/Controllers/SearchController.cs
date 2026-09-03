8fd2f89221d796b8724f56959faa6ec07fa2e483
strongerko7-netizen <your-email@example.com>
2026-09-03 22:32:13 +0200

    add SearchPortal

---
+++ SearchPortal/SearchPortal/Controllers/SearchController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SearchPortal.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index(string query)
+        {
+            ViewBag.Query = query;
+
+            return View();
+        }
+    }
+}
